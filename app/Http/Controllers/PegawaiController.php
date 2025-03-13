fbe792c8d12430ee73450e2a72d6136264a42a4f
yuliagitaps77 <gita.permata.saryy@gmail.com>
2025-03-13 22:21:38 +0700

    add validation input

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
-class PegawaiController extends Controller
+class PegawaiController
 {
+    use ValidatesRequests;
     public function index(Request $request){
         return $request->segment(2);
     }
@@ -13,11 +15,29 @@ class PegawaiController extends Controller
         return view('formulir');
     }
     
+    // public function proses(Request $request){
+    //     $nama = $request->input('nama');
+    //     $alamat = $request->input('alamat');
+    
+    //     return "Nama : ".$nama.", Alamat : ".$alamat;
+    // }
     public function proses(Request $request){
+        $messages = [
+            'required' => 'Input :attribute wajib diisi!',
+            'min' => 'Input :attribute harus diisi minimal :min karakter!',
+            'max' => 'Input :attribute harus diisi maksimal :max karakter!',
+        ];
+    
+        $this->validate($request, [
+            'nama' => 'required|min:5|max:20',
+            'alamat' => 'required|alpha'
+        ], $messages);
+    
         $nama = $request->input('nama');
         $alamat = $request->input('alamat');
     
         return "Nama : ".$nama.", Alamat : ".$alamat;
     }
     
+
 }
--- resources/views/formulir.blade.php
+++ resources/views/formulir.blade.php
@@ -2,13 +2,43 @@
 <html>
 <head>
     <title>Request dengan Input Laravel</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <form action="{{ url('/formulir/proses') }}" method="post">
-        @csrf
-        Nama : <input type="text" name="nama"><br/>
-        Alamat : <input type="text" name="alamat"><br/>
-        <input type="submit" value="Simpan">
-    </form>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="text-center">Form Validation dengan Laravel</h1>
+            <form action="/formulir/proses" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="nama" class="control-label">Nama Lengkap</label>
+                    <input type="text" id="nama" name="nama" class="form-control 
+                    {{ $errors->has('nama') ? ' is-invalid' : '' }}"
+                    placeholder="Nama Lengkap" value="{{ old('nama') }}">
+                    @if ($errors->has('nama'))
+                        <span class="text-danger small">
+                            <p>{{ $errors->first('nama') }}</p>
+                        </span>
+                    @endif
+                </div>
+                
+                <div class="form-group">
+                    <label for="alamat" class="control-label">Alamat</label>
+                    <textarea rows="3" id="alamat" name="alamat" class="form-control 
+                    {{ $errors->has('alamat') ? ' is-invalid' : '' }}"
+                    placeholder="Alamat">{{ old('alamat') }}</textarea>
+                    @if ($errors->has('alamat'))
+                        <span class="text-danger small">
+                            <p>{{ $errors->first('alamat') }}</p>
+                        </span>
+                    @endif
+                </div>
+
+                <input type="submit" value="Simpan" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
+</div>
 </body>
 </html>
